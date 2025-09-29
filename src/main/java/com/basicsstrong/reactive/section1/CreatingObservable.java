1fc2cb24f6eb310844c4165a1744a08eab95ee3b
musiciancoder <andresguitarra1980@gmail.com>
2025-09-29 11:21:25 -0300

    CreatingObservable

---
+++ src/main/java/com/basicsstrong/reactive/section1/CreatingObservable.java
@@ -0,0 +1,53 @@
+package com.basicsstrong.reactive.section1;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+
+public class CreatingObservable { //ojo, q hay mas formas de crear un iterabe, que no estan aqui pero ella las explica en a eccion con un powerpoint
+
+	public static void main(String[] args) {
+		
+		//create()
+		
+		Observable<Integer> source = Observable.create(
+
+				e ->{
+				e.onNext(10);
+				e.onNext(11);
+				e.onNext(12);
+				e.onComplete();
+				}
+				);
+		
+		
+		source.subscribe(System.out::println);
+		
+		
+		//just()
+		
+		
+		Observable<Integer> just = Observable.just(1,2,3);
+		
+		just.subscribe(System.out::println);
+		
+		
+		//fromIterable
+		
+		//List<String> list = List.of("Ram", "Shyam", "Mike");
+		List<String> list = new ArrayList<>();
+		list.add("Ram");
+		list.add("Shyam");
+		
+		@NonNull
+		Observable<String> fromIterable = Observable.fromIterable(list);
+		
+		
+		list.add("Rahin");
+		
+		fromIterable.subscribe(System.out::println);
+				
+	}
+
+}
