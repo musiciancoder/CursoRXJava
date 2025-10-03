8cf1fda7cde1540d4ee0be31b5176cefc4ce63d7
musiciancoder <andresguitarra1980@gmail.com>
2025-10-03 13:54:05 -0300

    onserveOn

---
+++ src/main/java/concurrencyandparalellization/FlatMapToAchieveAsync.java
@@ -0,0 +1,34 @@
+package concurrencyandparalellization;
+
+import java.time.LocalTime;
+
+import io.reactivex.rxjava3.core.Observable;
+
+
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class FlatMapToAchieveAsync {
+
+   public static void main(String[] args) throws InterruptedException {
+
+		Observable.just("Pasta", "Pizza", "Fries", "Curry", "Chow mein")
+		.flatMap(e -> Observable.just(e)
+				.subscribeOn(Schedulers.computation())
+				.map(str -> compute(str)))
+		.subscribe(System.out::println);
+		
+		Thread.sleep(7000);
+		
+		
+		
+	}
+
+	public static String compute(String element) throws InterruptedException {
+
+		String s  = element + " : Printed By : " + Thread.currentThread().getName() + " at : "+ LocalTime.now();
+		return s;
+	}
+
+}
+
+
